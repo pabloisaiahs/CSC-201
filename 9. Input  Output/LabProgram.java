955be817a0d2dff59e156da1fa610685957270c8
paulardis <85268281+pabloisaiahs@users.noreply.github.com>
2022-02-22 15:49:07 -0500

    Update LabProgram.java

--- 9. Input  Output/LabProgram.java
+++ 9. Input  Output/LabProgram.java
@@ -1,3 +1,10 @@
+/*9.7 LAB: File name change
+A photographer is organizing a photo collection about the national parks in the US and would like to annotate the information about each of the photos into 
+a separate set of files. Write a program that reads the name of a text file containing a list of photo file names. The program then reads the photo file names 
+from the text file, replaces the "_photo.jpg" portion of the file names with "_info.txt", and outputs the modified file names.
+
+Assume the unchanged portion of the photo file names contains only letters and numbers, and the text file stores one photo file name per line. 
+If the text file is empty, the program produces no output.*/
 import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.IOException;
